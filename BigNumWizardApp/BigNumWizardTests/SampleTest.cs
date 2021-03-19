f4725703b68bb99235806aa728b91c1bf594ac0a
Nikolay Savenko <savenkons@yandex.ru>
2021-03-19 23:39:44 +0300

    simple cast operators in BigNum

--- BigNumWizardApp/BigNumWizardTests/SampleTest.cs
+++ BigNumWizardApp/BigNumWizardTests/SampleTest.cs
@@ -6,16 +6,20 @@ namespace BigNumWizardTests
 {
 	public class SampleTest
 	{
+
 		[Fact]
 		public void Test1()
 		{
-			Assert.Equal("SomeAnotherString", BigNumWizardShared.ExampleClass.exampleString);
+			Assert.Equal("String from .NET Standart library", BigNumWizardShared.ExampleClass.exampleString);
 		}
 
 		[Fact]
 		public void Test2()
 		{
-			Assert.Equal("String from .NET Standart library", BigNumWizardShared.ExampleClass.exampleString);
+			string num = "12345";
+			BigNum bigNum = new BigNum(num);
+
+			Assert.Equal(num, ((int)bigNum).ToString());
 		}
 	}
 }
