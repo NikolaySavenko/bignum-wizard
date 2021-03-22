c4a1b88300dd5c214acd3125431e208c39b103db
Nikolay Savenko <savenkons@yandex.ru>
2021-03-22 16:11:33 +0300

    negative compare tests

---
+++ BigNumWizardApp/BigNumWizardTests/Compare/CompareNegativeTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BigNumWizardShared;
+
+namespace BigNumWizardTests.Compare
+{
+	public class CompareNegativeTests
+	{
+		[Fact]
+		public void EqualTest()
+		{
+			string num1 = "-123";
+			string num2 = "-123";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+			Assert.True(bigNum1 == bigNum2);
+		}
+
+		[Fact]
+		public void NotEqualTest()
+		{
+			string num1 = "-123";
+			string num2 = "-111";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+			Assert.True(bigNum1 != bigNum2);
+		}
+
+		[Fact]
+		public void BiggerTest()
+		{
+			string num1 = "-123";
+			string num2 = "-321";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+
+			Assert.True(bigNum1 > bigNum2);
+		}
+
+		[Fact]
+		public void BiggerEqualsTest()
+		{
+			string num1 = "-123";
+			string num2 = "-321";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+
+			Assert.True(bigNum1 >= bigNum2);
+		}
+
+		[Fact]
+		public void LessTest()
+		{
+			string num1 = "-321";
+			string num2 = "-123";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+
+			Assert.True(bigNum1 < bigNum2);
+		}
+
+		[Fact]
+		public void LessEqualsTest()
+		{
+			string num1 = "-321";
+			string num2 = "-123";
+			BigNum bigNum1 = new BigNum(num1);
+			BigNum bigNum2 = new BigNum(num2);
+
+			Assert.True(bigNum1 <= bigNum2);
+		}
+	}
+}
