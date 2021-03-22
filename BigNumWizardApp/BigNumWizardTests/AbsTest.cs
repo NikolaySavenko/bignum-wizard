bbe286252fa426ff33f667c9876ae62b985d1cd5
Nikolay Savenko <savenkons@yandex.ru>
2021-03-22 12:04:50 +0300

    abs tests

---
+++ BigNumWizardApp/BigNumWizardTests/AbsTest.cs
@@ -0,0 +1,39 @@
+﻿using BigNumWizardShared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BigNumWizardTests.Negative
+{
+	public class AbsTest
+	{
+		[Fact]
+		public void PositiveAbsTest()
+		{
+			var num = new BigNum("123");
+			Assert.Equal(new BigNum("123"), num.Absolute);
+		}
+
+		[Fact]
+		public void PositiveCharAbsTest()
+		{
+			var num = new BigNum("+123");
+			Assert.Equal(new BigNum("123"), num.Absolute);
+		}
+
+		[Fact]
+		public void NegativeAbsTest()
+		{
+			var num = new BigNum("-123");
+			Assert.Equal(new BigNum("123"), num.Absolute);
+		}
+
+		[Fact]
+		public void ZeroAbsTest()
+		{
+			var num = new BigNum("0");
+			Assert.Equal(new BigNum("0"), num.Absolute);
+		}
+	}
+}
