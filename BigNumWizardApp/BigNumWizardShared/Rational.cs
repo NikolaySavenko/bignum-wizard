52a43cfb179ec03a53aeabe30a29991f2bc3af18
Артём Иванов <artemivanov.nik2002@gmail.com>
2021-03-23 20:14:14 +0300

    Created Rational method

---
+++ BigNumWizardApp/BigNumWizardShared/Rational.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    class Rational
+    {
+        public static BigNum TRANS_Q_Z(BigNum fir, BigNum sec)
+        {
+            if (sec == new BigNum("1"))
+            {
+                return fir;
+            }
+            else
+            {
+                return sec;
+                // TODO make that shit better (example: 12/3)
+            }
+        }
+    }
+}
