2793bb26692905d7a89edeef37251d1731cfd70a
osiptsoff <osipczovnikita@gmail.com>
2021-03-23 23:34:34 +0300

    got updates from master; added 'public' to natural funcs; added rational sum and multiplication

--- BigNumWizardApp/BigNumWizardShared/Rational.cs
+++ BigNumWizardApp/BigNumWizardShared/Rational.cs
@@ -6,17 +6,25 @@ namespace BigNumWizardShared
 {
     public class Rational
     {
-        public static BigNum TRANS_Q_Z(BigNum fir, BigNum sec)
+        public static BigFraction ADD_QQ_Q(BigFraction fir, BigFraction sec)    // Сумма дробей (Осипцов, 0305)
         {
-            if (sec == new BigNum("1"))
-            {
-                return fir;
-            }
-            else
-            {
-                return sec;
-                // TODO make that shit better (example: 12/3)
-            }
+            BigFraction result = new BigFraction();
+                                                                                // Знаменатель - произведение знаменателей слагаемых
+            result.denominator = MUL_ZZ_Z(fir.denominator, sec.denominator);    // Числитель - сумма произведений знаменателей каждой дроби на числитель другой
+            result.nominator = SUM_ZZ_Z(MUL_ZZ_Z(fir.nominator, sec.denominator), MUL_ZZ_Z(fir.denominator, sec.nominator));//
+            return result;
+        }
+
+        public static BigFraction MUL_QQ_Q(BigFraction fir, BigFraction sec)    // Произведение дробей (Осипцов, 0305)
+        {
+            BigFraction result = new BigFraction();
+
+            result.nominator = MUL_ZZ_Z(fir.nominator, sec.nominator);          // Числитель - произведение числителей множителей
+            result.denominator = MUL_ZZ_Z(fir.denominator, sec.nominator);      // Знаменатель - произведение знаменателей множителей
+            return result;
         }
     }
 }
+
+// Возможны дальнейшие поправки по мере реализации дробей
+// BigFraction - возможная реализация класса дробей, где nominator - числитель, denominator - знаменатель
