e22d18bcb29bc0adedc889719a6d633489c01b53
VetaSolo <vetasolo75@gmail.com>
2021-03-24 21:23:47 +0300

    fix problem

--- BigNumWizardApp/BigNumWizardShared/N9.cs
+++ BigNumWizardApp/BigNumWizardShared/N9.cs
@@ -8,10 +8,10 @@ namespace BigNumWizardShared
     {
        public static BigNum SUB_NDN_N(BigNum A, BigNum B, int D) // N-9 Соловьева Елизавета 0310
         {
-            if (COM_NN_D(MUL_ND_N(A, D), B) == 2 || COM_NN_D(MUL_ND_N(A, D), B) == 0) //A*D >= B
-                return MUL_ND_N(A, D) - B; //A*D - B
-            else if (COM_NN_D(MUL_ND_N(B, D), A) == 2 || COM_NN_D(MUL_ND_N(A, B), A) == 0) //B*D >= A
-                return MUL_ND_N(B, D) - A; //B*D - A
+            if (Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), B) == 2 || Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), B) == 0) //A*D >= B
+                return N2_6.MUL_ND_N(A, D) - B; //A*D - B
+            else if (Natural1_5.COM_NN_D(N2_6.MUL_ND_N(B, D), A) == 2 || Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), A) == 0) //B*D >= A
+                return N2_6.MUL_ND_N(B, D) - A; //B*D - A
         }
     }
 }
