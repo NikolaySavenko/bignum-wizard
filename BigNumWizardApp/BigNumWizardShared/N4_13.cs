a8dc8673ed049ae53be9c10434ef9b0b1d4427dc
osiptsoff <osipczovnikita@gmail.com>
2021-03-24 18:36:09 +0300

    N-4 is ready at all

--- BigNumWizardApp/BigNumWizardShared/N4_13.cs
+++ BigNumWizardApp/BigNumWizardShared/N4_13.cs
@@ -2,17 +2,17 @@
 {
     public class N4_13
     {
-        public static BigNum ADD_NN_N(BigNum fir, BigNum sec)  // N-4, сумма натуральных чисел, Осипцов Никита 0305)
+        public static BigNum ADD_NN_N(BigNum fir, BigNum sec)  // N-4, сумма натуральных чисел, Осипцов Никита 0305
         {
-            if (COM_NN_D(fir, sec) == 1)
+            if (Natural1_5.COM_NN_D(fir, sec) == 1)
                 return sec + fir;                   // Если первое число меньше, прибавить первое ко второму
             else
                 return fir + sec;                   // Иначе прибавить второе к первому
         }
 
-        public static BigNum GCF_NN_N(BigNum fir, BigNum sec)  // N-13, НОД натуральных чисел, Осипцов Никита 0305)
+        public static BigNum GCF_NN_N(BigNum fir, BigNum sec)  // N-13, НОД натуральных чисел, Осипцов Никита 0305
         {
-            while (NZER_N_B(fir) != 1 && NZER_N_B(sec) != 1)     // Алгоритм Евклида
+            while (N2_6.NZER_N_B(fir) != "yes" && N2_6.NZER_N_B(sec) != "yes")     // Алгоритм Евклида
                 if (COM_NN_D(fir, sec) == 2)
                     fir = MOD_NN_N(fir, sec);
                 else
