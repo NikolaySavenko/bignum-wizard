434b0ca50f64e2b8552f2c72366370797a001459
VetaSolo <vetasolo75@gmail.com>
2021-03-24 21:26:50 +0300

    Fix Error CS0161

--- BigNumWizardApp/BigNumWizardShared/N9.cs
+++ BigNumWizardApp/BigNumWizardShared/N9.cs
@@ -9,9 +9,13 @@ namespace BigNumWizardShared
        public static BigNum SUB_NDN_N(BigNum A, BigNum B, int D) // N-9 Соловьева Елизавета 0310
         {
             if (Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), B) == 2 || Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), B) == 0) //A*D >= B
+            {
                 return N2_6.MUL_ND_N(A, D) - B; //A*D - B
+            }
             else if (Natural1_5.COM_NN_D(N2_6.MUL_ND_N(B, D), A) == 2 || Natural1_5.COM_NN_D(N2_6.MUL_ND_N(A, D), A) == 0) //B*D >= A
+            {
                 return N2_6.MUL_ND_N(B, D) - A; //B*D - A
+            }
         }
     }
 }
