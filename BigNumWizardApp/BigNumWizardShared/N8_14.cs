2f4b83737773c3586fa8fff878b021c6d61be502
stepanshvets <sh_stepa@mail.ru>
2021-03-25 23:09:29 +0300

    added N8 and Z4_5

--- BigNumWizardApp/BigNumWizardShared/N8_14.cs
+++ BigNumWizardApp/BigNumWizardShared/N8_14.cs
@@ -10,19 +10,16 @@ namespace BigNumWizardShared
     {
         public static BigNum MUL_NN_N(BigNum a, BigNum b) //N-8 умножение натуральных чисел Швец Степан 0305 
         {
-            BigNum res = new BigNum();
-            for (int i = 0, len = b.Lenght; i < len; i++)
-            {
-                res = ADD_NN_N(MUL_Nk_N(MUL_ND_N(a, b[len - i - 1]), i), res); //добавить имена классов
-            }
-            return res;
+            return a * b;
         }
 
+        /*
         public static BigNum LCM_NN_N(BigNum a, BigNum b) //N-14 нок натуральных чисел Швец Степан 0305 
         {
             BigNum res = new BigNum();
             res = DIV_NN_N(MUL_NN_N(a, b),GCF_NN_N(a, b)); //добавить имена классов
             return res;
         }
+        */
     }
 }
