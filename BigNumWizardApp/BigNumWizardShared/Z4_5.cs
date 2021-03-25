59f1f791b24dcb5fee7cfeb1f173b7276db35f7b
stepanshvets <sh_stepa@mail.ru>
2021-03-25 22:56:11 +0300

    added N8 and Z4_5

--- BigNumWizardApp/BigNumWizardShared/Z4_5.cs
+++ BigNumWizardApp/BigNumWizardShared/Z4_5.cs
@@ -10,17 +10,12 @@ namespace BigNumWizardShared
     {
         public static BigNum TRANS_N_Z(BigNum a) //Z-4 преобразование натурального в целое Швец Степан 0305
         {
-            BigNum res = new BigNum();
-            res = a;
-            return res;
+            return a;
         }
         public static BigNum TRANS_Z_N(BigNum a) //Z-5 преобразование целое в натуральное Швец Степан 0305
         {
-            //добавить: проверку, является ли число положительным if (a.Positive) и в блоке else добавить обработку исключений, если оно таковым не является
-
-            BigNum res = new BigNum();
-            res = a;
-            return res;
+            //добавить проверку, является ли число положительным if (a.Positive)
+            return a;
         }
     }
 }
