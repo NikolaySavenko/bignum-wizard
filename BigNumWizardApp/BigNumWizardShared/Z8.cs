28c1b3a42919a43fa2e42d3e1df74444aab5d47e
VetaSolo <vetasolo75@gmail.com>
2021-03-26 19:11:47 +0300

    class to public class

--- BigNumWizardApp/BigNumWizardShared/Z8.cs
+++ BigNumWizardApp/BigNumWizardShared/Z8.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BigNumWizardShared
 {
-    class Z8
+    public class Z8
     {
         public static BigNum MUL_ZZ_Z(BigNum A, BigNum B)  //// Z-8 Умножение целых чисел. Соловьева Елизавета 0310
         {
