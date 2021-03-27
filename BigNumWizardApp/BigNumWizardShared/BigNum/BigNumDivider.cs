ecf7104e26868a4b075525275f386d9ed5b97efc
Nikolay Savenko <savenkons@yandex.ru>
2021-03-27 12:51:56 +0300

    temporary divide func

---
+++ BigNumWizardApp/BigNumWizardShared/BigNum/BigNumDivider.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace BigNumWizardShared
+{
+	public partial class BigNum
+	{
+		public static BigNum operator /(BigNum a, BigNum b) {
+			return new BigNum((BigInteger.Parse((string)a) / BigInteger.Parse((string)b)).ToString());
+		}
+
+		public static BigNum operator %(BigNum a, BigNum b)
+		{
+			return new BigNum((BigInteger.Parse((string)a) % BigInteger.Parse((string)b)).ToString());
+		}
+
+		public BigNum Divide(BigNum divider, out BigNum remainer) {
+			if (divider > this) {
+				remainer = this;
+				return new BigNum(this);
+			} 
+			remainer = new BigNum("0");
+			return new BigNum("1");
+		}
+
+		private void RecursiveDivide() { 
+		
+		}
+	}
+}
