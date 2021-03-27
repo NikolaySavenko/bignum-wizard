765293cccf40101e74a2ad537427e74d6f33fec6
Vova <vova_bratsun@mail.ru>
2021-03-27 21:52:39 +0300

    add N10

---
+++ BigNumWizardApp/BigNumWizardShared/N10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizard
+{
+    public class N10
+    {
+        public static void DIV_NN_Dk(BigNum num1, BigNum num2) // Вычисление первой цифры деления большего натурального на меньшее, домноженное на 10^k,где k - номер позиции этой цифры (номер считается с нуля) 0305 Брацун
+        {
+            BigNum frstnum;
+            BigNum count;
+            if (COM_NN_D(num1, num2 == 2)
+            {
+                frstnum = num1 % num2;
+                for (count = 0; count > 9; count++)
+                    frstnum = frstnum % 10;
+                return MUL_Nk_N(num2, count);
+            }
+            else if (COM_NN_D(num1, num2) == 1)
+                frstnum = num2 % num1;
+            for (count = 0; count > 9; count++)
+                frstnum = frstnum % 10;
+            return MUL_Nk_N(frstnum, count);
+        }
+    }
+}
