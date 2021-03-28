a4b6a0c1fcbe776b9a9a30ad888c78de3836535e
Анастасия Пуха <pukha_an@mail.ru>
2021-03-28 13:49:17 +0300

    Corrected "public" class in N3_N7 and test for N6

--- BigNumWizardApp/BigNumWizardTests/Test_N6.cs
+++ BigNumWizardApp/BigNumWizardTests/Test_N6.cs
@@ -4,17 +4,17 @@ using BigNumWizardShared;
 
 namespace BigNumWizardTests
 {
-    public class Test_N6 //Lukina
+    public class Test_N6 //Lukina, Pukha
     {
         [Theory]
         [InlineData("12345679", 9, "111111111")]
-        [InlineData("987654321", 0, "0")] //не работает, возвращает массив нулей
+        [InlineData("987654321", 0, "0")] //ошибка, возвращает массив нулей, а не 0
         [InlineData("741852963", 3, "2225558889")]
 
         public void MultiplyByNumber(string target, byte num, string expexted)
         {
             var n1 = new BigNum(target);
-            n1.MultiplyByNumeral(num);
+            n1 = N2_6.MUL_ND_N(n1, num);
             Assert.Equal(n1, new BigNum(expexted));
         }
     }
