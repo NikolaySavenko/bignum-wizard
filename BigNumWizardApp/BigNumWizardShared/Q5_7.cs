8ea21f9efdd93a600f971e9088f8a2b03e56346f
osiptsoff <osipczovnikita@gmail.com>
2021-03-29 18:15:38 +0300

    added 'public' to class

--- BigNumWizardApp/BigNumWizardShared/Q5_7.cs
+++ BigNumWizardApp/BigNumWizardShared/Q5_7.cs
@@ -1,6 +1,6 @@
 ﻿namespace BigNumWizardShared
 {
-    class Q5_7
+    public class Q5_7
     {
         public static BigFraction ADD_QQ_Q(BigFraction fir, BigFraction sec) { return fir + sec; } // Q-5, сложение дробей, Осипцов Никита 0305
         //Сокращать результат, когда будет готов нужный подмодуль
