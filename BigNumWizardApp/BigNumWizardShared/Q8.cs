e6c3da1d2533e116c25587f5c24d653d35bb6af4
Марина Петракова <mrpetrakova2000@gmail.com>
2021-03-29 13:22:08 +0300

    Q8 - div.fractions

---
+++ BigNumWizardApp/BigNumWizardShared/Q8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    class Q8
+    {
+        public BigFraction DIV_QQ_Q(BigFraction first, BigFraction second) //Q-8 Деление дробей Петракова Марина 0305 
+        {
+            BigFraction fraction = first / second;
+            return fraction;
+        }
+    }
+}
