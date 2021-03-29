61c8c7b7aa35343ad503e1a721cc53bd7d96c6bb
Nikolay Savenko <savenkons@yandex.ru>
2021-03-29 22:52:48 +0300

    divide test

---
+++ BigNumWizardApp/BigNumWizardTests/DivideTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using BigNumWizardShared;
+
+namespace BigNumWizardTests
+{
+	public class DivideTest
+	{
+		[Theory]
+		[InlineData("123", "10", "1")]
+		[InlineData("123", "123", "1")]
+		[InlineData("124", "123", "1")]
+		public void SimpleDivide(string target, string num, string expected)
+		{
+			var n1 = new BigNum(target);
+			var n2 = new BigNum(num);
+			var div = n1 / n2;
+			Assert.Equal(div, new BigNum(expected));
+		}
+	}
+}
