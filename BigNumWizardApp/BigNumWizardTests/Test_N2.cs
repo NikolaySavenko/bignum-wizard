3b36a4cbd51cacc9a39c629b8e3e490f6e3e62bf
Александра Лукина <alexluk2001@yandex.ru>
2021-03-31 00:48:37 +0300

    Test N2

---
+++ BigNumWizardApp/BigNumWizardTests/Test_N2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using BigNumWizardShared;
+
+namespace BigNumWizardTests
+{
+    public class Test_N2 //Lukina
+    {
+        [Theory]
+        [InlineData("0", "no")]
+        [InlineData("98765432123456789", "yes")]
+        [InlineData("7", "yes")]
+        [InlineData("444444444", "yes")]
+        [InlineData("73456789876543245678900987654321", "yes")]
+
+        public void ZeroCheck(string target, string expexted)
+        {
+            var n1 = new BigNum(target);
+            var n2 = N2_6.NZER_N_B(n1);
+            Assert.Equal((string)n2, expexted);
+        }
+    }
+}
