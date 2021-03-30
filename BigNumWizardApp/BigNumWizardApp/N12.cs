71b22af57dddd718e4657109fe3bbadeab3e7021
Vova <vova_bratsun@mail.ru>
2021-03-30 20:04:45 +0300

    ADD N12...

---
+++ BigNumWizardApp/BigNumWizardApp/N12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumWizardShared
+{
+    public class N12
+    {
+        public static BigNum DIV_NN_Dk(BigNum a, BigNum b)
+        {
+            if (Natural1_5.COM_NN_D(a, b) == 2 || Natural1_5.COM_NN_D(a, b) == 0)
+                return a % b;
+            else
+                return b % a;
+        }
+    }
+}
