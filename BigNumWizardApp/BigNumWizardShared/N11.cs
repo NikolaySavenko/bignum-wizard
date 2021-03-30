e34ae841522af5b3e6890e1bfa5fc9127fbe437b
Анастасия Пуха <pukha_an@mail.ru>
2021-03-30 19:13:47 +0300

    fin ver N11

--- BigNumWizardApp/BigNumWizardShared/N11.cs
+++ BigNumWizardApp/BigNumWizardShared/N11.cs
@@ -6,21 +6,9 @@ namespace BigNumWizardShared
 {
     public class N11
     {
-        public static BigNum DIV_NN_N(BigNum A, BigNum B) // Частное от деления большего натурального числа на меньшее или равное натуральное с остатком(делитель отличен от нуля)
+        public static BigNum DIV_NN_N(BigNum A, BigNum B, out BigNum remainer) // Частное от деления большего натурального числа на меньшее или равное натуральное с остатком(делитель отличен от нуля)
         {
-            BigNum Q , R;
-            if (A>=B)
-            {
-                Q = A / B; // целая часть
-                R = A % B; // остаток
-                return Q;
-            }
-            else
-            {
-                Q = B / A;
-                R = B % A;
-                return R;
-            }
+            return A.Divide(B, out remainer);
         }
     }
 }
