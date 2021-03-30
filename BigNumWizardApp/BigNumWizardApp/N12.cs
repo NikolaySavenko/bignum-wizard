21ba0ec8202090f17676a0b95be6dffc370dfbd2
Vova <vova_bratsun@mail.ru>
2021-03-30 20:11:37 +0300

    corrected

--- BigNumWizardApp/BigNumWizardApp/N12.cs
+++ BigNumWizardApp/BigNumWizardApp/N12.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace BigNumWizardShared
 {
-    public class N12
+    public class N12 //Bratsun
     {
-        public static BigNum DIV_NN_Dk(BigNum a, BigNum b)
+        public static BigNum DIV_NN_Dk(BigNum a, BigNum b) //Остаток от деления большего натурального числа на меньшее или равное натуральное с остатком(делитель отличен от нуля)
         {
-            if (Natural1_5.COM_NN_D(a, b) == 2 || Natural1_5.COM_NN_D(a, b) == 0)
+            if (Natural1_5.COM_NN_D(a, b) == 2 || Natural1_5.COM_NN_D(a, b) == 0) 
                 return a % b;
             else
                 return b % a;
