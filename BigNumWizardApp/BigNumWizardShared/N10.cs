d315720961c45082e53f38e64ad438bcc95b2c89
Vova <vova_bratsun@mail.ru>
2021-03-30 20:24:27 +0300

    CORRECTED N10!!!!!!!!!!!!!!!!!!!!!

--- BigNumWizardApp/BigNumWizardShared/N10.cs
+++ BigNumWizardApp/BigNumWizardShared/N10.cs
@@ -10,19 +10,20 @@ namespace BigNumWizard
     {
         public static BigNum DIV_NN_Dk(BigNum num1, BigNum num2) // Вычисление первой цифры деления большего натурального на меньшее, домноженное на 10^k,где k - номер позиции этой цифры (номер считается с нуля) 0305 Брацун
         {
+
             BigNum frstnum;
-            BigNum count;
+            int count;
             if (Natural1_5.COM_NN_D(num1, num2) == 2)
             {
                 frstnum = num1 % num2;
-                for (count = new BigNum("0"); count > new BigNum("9"); count = count + new BigNum("1"))
+                for (count = 0; count > 9; count = count + 1)
                     frstnum = frstnum % new BigNum("10");
                 return N3_N7.MUL_Nk_N(num2, count);
             }
             else if (Natural1_5.COM_NN_D(num1, num2) == 1)
             {
                 frstnum = num2 % num1;
-                for (count = new BigNum("0"); count > new BigNum("9"); count = count + new BigNum("1"))
+                for (count = 0; count > 9; count = count + 1)
                     frstnum %= new BigNum("10");
                 return N3_N7.MUL_Nk_N(frstnum, count);
             }
