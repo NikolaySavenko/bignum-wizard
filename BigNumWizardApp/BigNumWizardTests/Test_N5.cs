b8ad514d7d6e7bb3a632e91c654cdbf2176fdfcf
Александра Лукина <alexluk2001@yandex.ru>
2021-03-30 20:12:13 +0300

    Test N5

---
+++ BigNumWizardApp/BigNumWizardTests/Test_N5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using BigNumWizardShared;
+
+namespace BigNumWizardTests
+{
+    public class Test_N5 //Lukina
+    {
+        [Theory]
+        [InlineData("12345", "12344", "1")]
+        [InlineData("123", "12", "111")]
+        [InlineData("9990", "9990", "0")]
+        [InlineData("9", "7", "2")]
+        [InlineData("99999999999", "55555555555", "44444444444")]
+        [InlineData("7", "9", "2")]
+        [InlineData("9876543", "7654321", "2222222")]
+        [InlineData("9876543", "7", "9876536")]
+        [InlineData("98", "777777777777", "777777777679")]
+        [InlineData("8888888888888", "8888888888888", "0")]
+
+        public void Substraction(string target1, string target2, string expexted)
+        {
+            var n1 = new BigNum(target1);
+            var n2 = new BigNum(target2);
+            var n3 = Natural1_5.SUB_NN_N(n1, n2);
+            Assert.Equal(new BigNum((string)n3), new BigNum(expexted));
+        }
+    }
+}
