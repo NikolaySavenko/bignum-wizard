293244ef12fbc290271f4b4568f171deb9281b25
VetaSolo <vetasolo75@gmail.com>
2021-03-31 13:07:27 +0300

    Fix someting

--- BigNumWizardApp/BigNumWizardShared/Z9.cs
+++ BigNumWizardApp/BigNumWizardShared/Z9.cs
@@ -9,9 +9,13 @@ namespace BigNumWizardShared
         public static BigNum DIV_NN_N(BigNum A, BigNum B) // Частное от деления целого на целое (делитель отличен от нуля) Соловьева 0310
         {
             if (z2_3.POZ_Z_D(A) != 0 || B > A)
-                return A/B;
+                return A / B;
+            else if (z2_3.POZ_Z_D(B) != 0 || A > B)
+                return B / A;
             else
-                return B/A;
+            { 
+                throw new Exception("Something went wrong");
+            }
         }
 
     }
