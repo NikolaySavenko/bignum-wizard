4a81740abaeb5ac10b79396d43a11735531a5e80
Stepan <flygovno@mail.ru>
2021-03-31 19:42:59 +0300

    Added new pages: for one and three numbers Added N9 Added button for show result

--- BigNumWizardApp/BigNumWizardApp/MainPage.xaml.cs
+++ BigNumWizardApp/BigNumWizardApp/MainPage.xaml.cs
@@ -66,13 +66,46 @@ namespace BigNumWizardApp
                         nvMain.Header = "Умножение";
                         break;
                     case "PageDivide":
-                        TwoNumbersPage.Value2 = "1"; // default value
                         TwoNumbersPage.TargetFunctionDelegate actionDivide = (string param1, string param2) =>
                         {
                             return (string)(new BigNum(param1) / new BigNum(param2));
                         };
                         ContentFrame.Navigate(typeof(TwoNumbersPage), actionDivide);
                         nvMain.Header = "Деление";
+                        break;
+                    case "PageComparison":
+                        TwoNumbersPage.TargetFunctionDelegate actionComparison = (string param1, string param2) =>
+                        {
+                            return Natural1_5.COM_NN_D(new BigNum(param1), new BigNum(param2)).ToString();
+                        };
+                        ContentFrame.Navigate(typeof(TwoNumbersPage), actionComparison);
+                        nvMain.Header = "Сравнение";
+                        break;
+                    case "PageN9":
+                        ThreeNumbersPage.TargetFunctionDelegate actionN9 = (string param1, string param2, string param3) =>
+                        {
+                            byte value = param3 != "" ? byte.Parse(param3) : (byte)0;
+                            return (string)N9.SUB_NDN_N(new BigNum(param1), new BigNum(param2), value);
+                        };
+                        ContentFrame.Navigate(typeof(ThreeNumbersPage), actionN9);
+                        nvMain.Header = "Подмодуль N9";
+                        break;
+                    case "PageN11":
+                        ThreeNumbersPage.TargetFunctionDelegate actionN11 = (string param1, string param2, string param3) =>
+                        {
+                            var value = new BigNum(param3);
+                            return (string)N11.DIV_NN_N(new BigNum(param1), new BigNum(param2), out value);
+                        };
+                        ContentFrame.Navigate(typeof(ThreeNumbersPage), actionN11);
+                        nvMain.Header = "Подмодуль N11";
+                        break;
+                    case "PageCheckZero":
+                        OneNumberPage.TargetFunctionDelegate actionCheckZero = (string param1) =>
+                        {
+                            return N2_6.NZER_N_B(new BigNum(param1));
+                        };
+                        ContentFrame.Navigate(typeof(OneNumberPage), actionCheckZero);
+                        nvMain.Header = "Проверка на ноль";
                         break;
                 }
             }
--- BigNumWizardApp/BigNumWizardApp/ThreeNumbersPage.xaml.cs
+++ BigNumWizardApp/BigNumWizardApp/ThreeNumbersPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.UI.Popups;
@@ -11,48 +10,59 @@ namespace BigNumWizardApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class TwoNumbersPage : Page
+    public sealed partial class ThreeNumbersPage : Page
     {
-        public delegate string TargetFunctionDelegate(string param1, string param2);
+        public delegate string TargetFunctionDelegate(string param1, string param2, string param3);
 
         private TargetFunctionDelegate func;
 
         private string Value1 { get; set; } = "0";
-        public static string Value2 { get; set; } = "0";
+        private static string Value2 { get; set; } = "0";
+        private string Value3 { get; set; } = "0";
 
-        public TwoNumbersPage()
+        public ThreeNumbersPage()
         {
             this.InitializeComponent();
             numberBox1.TextChanged += NumberBox1_TextChanged1;
             numberBox2.TextChanged += NumberBox2_TextChanged1;
+            numberBox3.TextChanged += NumberBox3_TextChanged1;
+
         }
 
         private void NumberBox2_TextChanged1(object sender, TextChangedEventArgs e)
         {
             TextBox box = sender as TextBox;
             Value2 = box != null ? box.Text : Value2;
+            IvokeAction();
+        }
+
+        private void NumberBox3_TextChanged1(object sender, TextChangedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            Value3 = box != null ? box.Text : Value3;
+            IvokeAction();
         }
 
         private void NumberBox1_TextChanged1(object sender, TextChangedEventArgs e)
         {
             TextBox box = sender as TextBox;
             Value1 = box != null ? box.Text : Value1;
+            IvokeAction();
         }
 
-        async private void InvokeAction()
+        async private void IvokeAction()
         {
             try
             {
-                textBox.Text = func(Value1, Value2);
-            }      
-            catch (Exception err)
+                textBox.Text = func(Value1, Value2, Value3);
+            }
+            catch (Exception e)
             {
-                var messageDialog = new MessageDialog(err.Message.ToString());
+                var messageDialog = new MessageDialog(e.Message.ToString());
                 await messageDialog.ShowAsync();
             }
-            
-        }
 
+        }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
