08607c772715cf6083f9407d455fe2c6d2dcf89e
VetaSolo <vetasolo75@gmail.com>
2021-03-31 13:42:11 +0300

    N11 add in Z9

--- BigNumWizardApp/BigNumWizardShared/Z9.cs
+++ BigNumWizardApp/BigNumWizardShared/Z9.cs
@@ -6,14 +6,24 @@ namespace BigNumWizardShared
 {
     public class Z9
     {
-        public static BigNum DIV_ZZ_Z(BigNum A, BigNum B) // Частное от деления целого на целое (делитель отличен от нуля) Соловьева 0310
+        public static BigNum DIV_ZZ_Z(BigNum A, BigNum B, out BigNum remainer) // Частное от деления целого на целое (делитель отличен от нуля) Соловьева 0310
         {
             if (z2_3.POZ_Z_D(A) != 0 || B > A)
-                return A / B;
+            {
+                BigNum C;
+                C = N11.DIV_NN_N(A, B, out remainer);
+                return C;
+                //return A / B;
+            }
             else if (z2_3.POZ_Z_D(B) != 0 || A > B)
-                return B / A;
+            {
+                BigNum C;
+                C = N11.DIV_NN_N(B, A, out remainer);
+                return C;
+            }
+                //return B / A;
             else
-            { 
+            {
                 throw new Exception("Something went wrong");
             }
         }
