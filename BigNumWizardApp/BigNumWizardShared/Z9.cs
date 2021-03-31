88991f7683bbd5ab05a554fcc1e00b89809d69cb
VetaSolo <vetasolo75@gmail.com>
2021-03-31 12:53:26 +0300

    Add z9

---
+++ BigNumWizardApp/BigNumWizardShared/Z9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    public class Z9
+    {
+        public static BigNum DIV_NN_N(BigNum A, BigNum B) // Частное от деления целого на целое (делитель отличен от нуля) Соловьева 0310
+        {
+            if (z2_3.POZ_Z_D(A) != 0 || B > A)
+                return A/B;
+            else
+                return B/A;
+        }
+
+    }
+}
