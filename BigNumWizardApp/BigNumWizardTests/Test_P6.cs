9dafe3e922975f9a448854e08683e31ec2b605c3
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-01 23:16:40 +0300

    Test for P6

--- BigNumWizardApp/BigNumWizardTests/Test_P6.cs
+++ BigNumWizardApp/BigNumWizardTests/Test_P6.cs
@@ -17,6 +17,8 @@ namespace BigNumWizardTests
             Assert.Equal(actual, res);
         }  
 
+        //Question. Polynom's coeff. mustn't be BigNum?
+
         public static IEnumerable<object[]> Data
         {
             get
@@ -25,7 +27,7 @@ namespace BigNumWizardTests
                 {
                     new object[] { 1, new List<int>() { 1, 2 }, 1 },
                     new object[] { 6, new List<int>() { 534, 1775, 98888, 976, 180, 432, 0 }, 6},
-                    new object[] { 5, new List<int>() { 4543, 21, 1111, 5448, 866, 2342 }, 5},
+                    new object[] { 5, new List<int>() { 4543, 21, 1111, 5448, 8462, 2342 }, 5},
                     new object[] { 0, new List<int>() { 126545 }, 0},
                     new object[] { 8, new List<int>() { 0, 12, 832, 0, 0, 12274246, 0, 900, 126 }, 8},
                     new object[] { 2, new List<int>() { 0, 0, 99132 }, 2}
