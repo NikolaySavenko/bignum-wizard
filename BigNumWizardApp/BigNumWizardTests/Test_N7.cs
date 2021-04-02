f9e792f9c8d108becfe0c8e892ab341b921d42cb
Vova <vova_bratsun@mail.ru>
2021-04-02 13:23:53 +0300

    N7 corrected

--- BigNumWizardApp/BigNumWizardTests/Test_N7.cs
+++ BigNumWizardApp/BigNumWizardTests/Test_N7.cs
@@ -9,12 +9,12 @@ namespace BigNumWizardTests
 		[Theory]
 		[InlineData("1", 3, "1000")]
 		[InlineData("98764242321", 5, "9876424232100000")] 
-		[InlineData("-222222222222222", 11, "-22222222222222200000000000")]
+		[InlineData("222222222222222", 11, "22222222222222200000000000")]
 		[InlineData("900000000000000", 1, "9000000000000000")]
 		[InlineData("13", 1, "130")]
 		[InlineData("189301", 10, "1893010000000000")]
 		[InlineData("99999999999", 17, "9999999999900000000000000000")]
-
+		[InlineData("123456789", 0, "123456789")]
 		public void tenk_Int_Test(string num1, int k, string expected) 
         {
 			var n1 = new BigNum(num1);
