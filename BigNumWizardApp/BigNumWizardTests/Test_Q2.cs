71f4ae97dea36622a44f5e0d6679b38cde320646
VetaSolo <vetasolo75@gmail.com>
2021-04-02 12:00:33 +0300

    add new tests

--- BigNumWizardApp/BigNumWizardTests/Test_Q2.cs
+++ BigNumWizardApp/BigNumWizardTests/Test_Q2.cs
@@ -13,6 +13,12 @@ namespace BigNumWizardTests
         [InlineData("444444444", "222222222", "Yes")]
         [InlineData("734567898765432456789009876543210", "5", "Yes")]
         [InlineData("50959854608945687", "2", "No")]
+        [InlineData("-1234576785554433766543245678900987654321000", "1000", "Yes")]
+        [InlineData("-509598546089456879000000000000000000000000000000000000000000", "-1000000000000000000000000000000000000000", "Yes")]
+        [InlineData("-123456", "2", "Yes")]
+        [InlineData("66666666666666666666", "-3", "Yes")]
+        [InlineData("50959854608945687", "-23434453455456565465655675567756", "No")]
+
 
         public void IntCheck(string target1, string target2, string expexted)
         {
