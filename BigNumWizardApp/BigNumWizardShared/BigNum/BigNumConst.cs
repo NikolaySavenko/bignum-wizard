c6a030cba9a8ca522a1e3eb1e5cf1d1460fb7794
Nikolay Savenko <savenkons@yandex.ru>
2021-04-05 12:29:16 +0300

    added simple consts

---
+++ BigNumWizardApp/BigNumWizardShared/BigNum/BigNumConst.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigNumWizardShared
+{
+	public partial class BigNum
+	{
+		//
+		// Summary:
+		//     Gets a value that represents the number one (1).
+		//
+		// Returns:
+		//     An object whose value is one (1).
+		public static BigNum One { get; } = new BigNum("1");
+		//
+		// Summary:
+		//     Gets a value that represents the number negative one (-1).
+		//
+		// Returns:
+		//     An BigNum whose value is negative one (-1).
+		public static BigNum MinusOne { get; } = new BigNum("-1");
+		//
+		// Summary:
+		//     Gets a value that represents the number 0 (zero).
+		//
+		// Returns:
+		//     An BigNum whose value is 0 (zero).
+		public static BigNum Zero { get; } = new BigNum("0");
+		//
+		// Summary:
+		//     Gets a value that represents the number 10 (ten).
+		//
+		// Returns:
+		//     An BigNum whose value is 10 (zero).
+		public static BigNum Ten { get; } = new BigNum("10");
+	}
+}
