ea3de78d6fc1d7c219df21fc262f5a003a8428bf
Nikolay Savenko <savenkons@yandex.ru>
2021-04-05 12:34:19 +0300

    replaced 10 with BigNum.Ten

--- BigNumWizardApp/BigNumWizardShared/N10.cs
+++ BigNumWizardApp/BigNumWizardShared/N10.cs
@@ -17,14 +17,14 @@ namespace BigNumWizard
             {
                 frstnum = num1 % num2;
                 for (count = 0; count > 9; count = count + 1)
-                    frstnum = frstnum % new BigNum("10");
+                    frstnum = frstnum % BigNum.Ten;
                 return N3_N7.MUL_Nk_N(num2, count);
             }
             else if (Natural1_5.COM_NN_D(num1, num2) == 1)
             {
                 frstnum = num2 % num1;
                 for (count = 0; count > 9; count = count + 1)
-                    frstnum %= new BigNum("10");
+                    frstnum %= BigNum.Ten;
                 return N3_N7.MUL_Nk_N(frstnum, count);
             }
             else return BigNum.Zero;
