e33858707b0709b4c4dc614b0bc9fa01862827f8
osiptsoff <osipczovnikita@gmail.com>
2021-04-05 22:58:20 +0300

    Q5 added

--- BigNumWizardApp/BigNumWizardShared/Q5_7.cs
+++ BigNumWizardApp/BigNumWizardShared/Q5_7.cs
@@ -2,8 +2,22 @@
 {
     public class Q5_7
     {
-        public static BigFraction ADD_QQ_Q(BigFraction fir, BigFraction sec) { return fir + sec; } // Q-5, сложение дробей, Осипцов Никита 0305
-        //Сокращать результат, когда будет готов нужный подмодуль
+        public static BigFraction ADD_QQ_Q(BigFraction fir, BigFraction sec)    // Q-5, сложение дробей, Осипцов Никита 0305
+        {
+            var result = fir + sec; // Получить несокращённую дробь-сумму
+
+            if (result.Nom == BigNum.Zero)                              // Если в числителе ноль, то окончательный результат имеет вид 0/1
+                result = new BigFraction(BigNum.Zero, BigNum.One);
+            else                                                        // Иначе дробь сокращается
+            {
+                if (result.Positive == true)
+                    result = Q1.RED_Q_Q(result);
+                else
+                    result = Q1.RED_Q_Q(result).GetOppositeSigned();
+            }
+            return result;
+        }
+       
         public static BigFraction MUL_QQ_Q(BigFraction fir, BigFraction sec) { return fir * sec; } // Q-7, умножение дробей, Осипцов Никита 0305
     }
 }
