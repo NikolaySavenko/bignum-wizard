76d01cb8fd66c0cdd953ecc8cfee2aa9f67faa6c
Анастасия Пуха <pukha_an@mail.ru>
2021-04-06 19:57:25 +0300

    test for P12

---
+++ BigNumWizardApp/BigNumWizardTests/Test_P12.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using BigNumWizardShared;
+using System.Collections.Generic;
+
+namespace BigNumWizardTests
+{
+    public class Test_P12 //Solovieva
+    {
+        [Theory, MemberData(nameof(Data))]
+
+        public static void Derivative(int m, List<BigFraction> C, Polynomial res)
+        {
+            var actual = P12.DER_P_P(m, C);
+            Assert.Equal(actual, res);
+        }
+
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[]
+                    {
+                        1,
+                        new List<BigFraction>() { new BigFraction(BigNum.One), new BigFraction(new BigNum("2")) },
+                        new Polynomial(0,new List<BigFraction>(){new BigFraction(new BigNum("2"))})
+                    },
+
+                    new object[]
+                    {
+                        2,
+                        new List<BigFraction>() { new BigFraction(BigNum.One), new BigFraction(BigNum.Zero), new BigFraction(new BigNum("2344")) },
+                        new Polynomial(1,new List<BigFraction>(){new BigFraction(BigNum.Zero),new BigFraction(new BigNum("4688"))})
+                    },
+
+                    new object[]
+                    {
+                        3,
+                        new List<BigFraction>() { new BigFraction(new BigNum("34798547874")), new BigFraction(new BigNum("4")), new BigFraction(new BigNum("7654")), new BigFraction(new BigNum("2344")) },
+                        new Polynomial(2,new List<BigFraction>(){new BigFraction(new BigNum("4")), new BigFraction(new BigNum("15308")), new BigFraction(new BigNum("7032"))})
+                    },
+                };
+            }
+        }
+
+    }
+}
