f8c2b0144841da3e3515131d37cdb4869ebf4bf3
Алексей Кабанов <arkabanov@stud.etu.ru>
2021-04-07 22:53:15 +0300

    Add Z10 and Test_Z10. Modified Natural1_5.SUB_NN_N

---
+++ BigNumWizardApp/BigNumWizardShared/Z10.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    public class Z10
+    {
+        public static BigNum MOD_ZZ_Z(BigNum fir, BigNum sec, out BigNum remainer)
+        {
+            BigNum tmp_fir;
+            BigNum tmp_sec;
+
+            if (z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 2)
+            {
+                tmp_fir = Z9.DIV_ZZ_Z(fir, sec, out remainer);
+                tmp_sec = Z8.MUL_ZZ_Z(tmp_fir, sec);
+
+                remainer = Z7.SUB_ZZ_Z(fir, tmp_sec);
+                
+                return remainer;
+            }
+            else if(z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 2)
+            {
+                tmp_fir = z2_3.MUL_ZM_Z(fir);
+
+                tmp_fir = Z9.DIV_ZZ_Z(tmp_fir, sec, out remainer);
+                tmp_sec = Z8.MUL_ZZ_Z(tmp_fir + BigNum.One, sec);
+
+                remainer = Z7.SUB_ZZ_Z(tmp_sec, fir);
+                return remainer;
+            }
+            else if(z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 1)
+            {
+                tmp_sec = z2_3.MUL_ZM_Z(sec);
+
+                tmp_fir = Z9.DIV_ZZ_Z(fir, tmp_sec, out remainer);
+                tmp_sec = Z8.MUL_ZZ_Z(tmp_fir + BigNum.One, sec);
+
+                remainer = Z7.SUB_ZZ_Z(tmp_sec, fir);
+                return remainer;
+            }
+            else if(z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 1)
+            {
+                tmp_fir = z2_3.MUL_ZM_Z(fir);
+                tmp_sec = z2_3.MUL_ZM_Z(sec);
+
+                remainer = Z9.DIV_ZZ_Z(tmp_sec, tmp_sec, out remainer);
+                remainer = Z8.MUL_ZZ_Z(remainer + BigNum.One, tmp_sec);
+
+                remainer = Z7.SUB_ZZ_Z(tmp_fir, remainer);
+                return remainer;
+            }
+            else
+                throw new Exception("Something went wrong");
+        }
+    }
+}
---
+++ BigNumWizardApp/BigNumWizardTests/Test_Z10.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using BigNumWizardShared;
+
+namespace BigNumWizardTests
+{
+    public class Test_Z10
+    {
+        [Theory]
+        [InlineData("100", "2", "0")]
+        [InlineData("5000", "3000", "2000")]
+        [InlineData("500000001", "2", "1")]
+        [InlineData("45", "100", "45")]
+        [InlineData("-100", "50", "0")]
+        [InlineData("-100", "55", "10")]
+
+
+        public void Rest_of_divide(string target, string num, string expexted)
+        {
+
+            var n1 = new BigNum(target);
+            var n2 = new BigNum(num);
+            var sum = Z10.MOD_ZZ_Z(n1, n2, out _);
+
+            Assert.Equal(sum, new BigNum(expexted));
+        }
+    }
+}
