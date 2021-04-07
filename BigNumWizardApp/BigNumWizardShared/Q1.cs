8af670859c6d2f2ff05ad1d382775a33447c6461
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-07 23:31:47 +0300

    test for Q1 (doesn't work for very long numbers, check it after)

--- BigNumWizardApp/BigNumWizardShared/Q1.cs
+++ BigNumWizardApp/BigNumWizardShared/Q1.cs
@@ -8,12 +8,14 @@ namespace BigNumWizardShared
     {
         public static BigFraction RED_Q_Q(BigFraction fract) //Q-1 Сокращение дроби Петракова Марина 0305
         {
-            BigNum abs_nom = Absolute.ABS_Z_N(fract.Nom);
-            BigNum abs_denom = Absolute.ABS_Z_N(fract.Denom);
-            BigNum gcf = N4_13.GCF_NN_N(abs_nom, abs_denom);
+            BigNum gcf = N4_13.GCF_NN_N(fract.Nom, fract.Denom);
             BigNum new_nom = Z9.DIV_ZZ_Z(fract.Nom, gcf, out BigNum remainer0);
             BigNum new_denom = Z9.DIV_ZZ_Z(fract.Denom, gcf, out BigNum remainer1);
             BigFraction result = new BigFraction(new_nom, new_denom);
+            if (fract.Positive == false)
+            {
+                result = result.GetOppositeSigned();
+            }
             return result;
         }
     }
