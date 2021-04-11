9f5c3f83a2a3d5de214e9bfff130ae8929bb79b8
stepanshvets <sh_stepa@mail.ru>
2021-04-11 13:55:09 +0300

    Added P9

---
+++ BigNumWizardApp/BigNumWizardShared/P9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    public class P9 //Shvets
+    {
+        public static Polynomial DIV_PP_P(int m1, List<BigFraction> C1, int m2, List<BigFraction> C2)
+        {
+            int d = m1 - m2;
+
+            var resultSeniorDegree = new BigNum(d.ToString());
+            var resultOdds = new List<BigFraction>();
+
+            for (int i = 0; i < d + 1; i++)
+            {
+                resultOdds.Add(Q8.DIV_QQ_Q(C1[0], C2[0]));
+
+                for (int j = 0; j < m2; j++)
+                {
+                    C1[j] = Q6.SUB_QQ_Q(C1[j], Q5_7.MUL_QQ_Q(C2[j], resultOdds[i]));
+                }
+                C1.RemoveAt(0);
+            }
+
+            var resultPolynom = new Polynomial(resultSeniorDegree, resultOdds);
+
+            return resultPolynom;
+        }
+    }
+}
