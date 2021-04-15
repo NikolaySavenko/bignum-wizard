bf95ee223e16963d261fa2d54a7359440f615a22
Артём Иванов <artemivanov.nik2002@gmail.com>
2021-04-15 17:34:09 +0300

    Added P11

---
+++ BigNumWizardApp/BigNumWizardShared/P11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BigNumWizardShared
+{
+    public class P11  //P-11 НОД многочленов Иванов Артём 0305
+    {
+        public static Polynomial GCF_PP_P(BigNum m1, List<BigFraction> C1, BigNum m2, List<BigFraction> C2)
+        {
+            var r1 = new Polynomial(m1, C1);
+            var r2 = new Polynomial(m2, C2);
+            var tmp = new Polynomial(m2, C2);
+            while (r2.SeniorDegree != BigNum.Zero || r2.Odds[0] != new BigFraction(BigNum.Zero))
+            {
+                tmp = P10.MOD_PP_P(r1, r2);
+                r1 = r2;
+                r2 = tmp;
+            }
+            return r2;
+        }
+    }
+}
