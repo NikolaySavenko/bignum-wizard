a298e25e06f6a876cc68fd9942bbeb17e356e175
stepanshvets <sh_stepa@mail.ru>
2021-04-19 14:44:20 +0300

    Edited P9

--- BigNumWizardApp/BigNumWizardShared/P9.cs
+++ BigNumWizardApp/BigNumWizardShared/P9.cs
@@ -14,15 +14,16 @@ namespace BigNumWizardShared
                 var resultSeniorDegree = m1 - m2;
                 var resultOdds = new List<BigFraction>();
 
-                var N = C1.GetRange(0, C1.Count - 1);
+                var N = C1.GetRange(0, C1.Count);
 
                 for (var i = BigNum.Zero; i < resultSeniorDegree + BigNum.One; i = i + BigNum.One)
                 {
                     resultOdds.Add(Q8.DIV_QQ_Q(N[0], C2[0]));
 
-                    for (var j = BigNum.Zero; j < m2; j = j + BigNum.One)
+                    int n = m2.ConvertToInt();
+                    for (int j = 0; j < n + 1; j++)
                     {
-                        N[j.ConvertToInt()] = Q6.SUB_QQ_Q(N[j.ConvertToInt()], Q5_7.MUL_QQ_Q(C2[j.ConvertToInt()], resultOdds[i.ConvertToInt()]));
+                        N[j] = Q6.SUB_QQ_Q(N[j], Q5_7.MUL_QQ_Q(C2[j], resultOdds[i.ConvertToInt()]));
                     }
                     N.RemoveAt(0);
                 }
