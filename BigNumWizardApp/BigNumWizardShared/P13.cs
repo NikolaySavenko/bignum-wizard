0aed7f539a2fc768712eb5bbbde6d4c738951fac
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-20 20:42:43 +0300

    added empty p_13 test, fixed typo

--- BigNumWizardApp/BigNumWizardShared/P13.cs
+++ BigNumWizardApp/BigNumWizardShared/P13.cs
@@ -7,8 +7,8 @@ namespace BigNumWizardShared
         public static Polynomial NMR_P_P(BigNum m, List<BigFraction> C)
         {
             var C_der = P12.DER_P_P(m, C);
-            var devider = P11.GCF_PP_P(m, C, C_der.SeniorDegree, C_der.Odds);
-            var result = P9.DIV_PP_P(m, C, devider.SeniorDegree, devider.Odds);
+            var divider = P11.GCF_PP_P(m, C, C_der.SeniorDegree, C_der.Odds);
+            var result = P9.DIV_PP_P(m, C, divider.SeniorDegree, divider.Odds);
             return result;
         }
     }
---
+++ BigNumWizardApp/BigNumWizardTests/Test_P13.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using BigNumWizardShared;
+using System.Collections.Generic;
+
+namespace BigNumWizardTests
+{
+    public class Test_P13
+    {
+        [Theory, MemberData(nameof(Data))]
+        public static void ToSimple(BigNum m, List<BigFraction> a, Polynomial res)
+        {
+            var actual = P13.NMR_P_P(m, a);
+            Assert.Equal(res, actual);
+        }
+        public static IEnumerable<object[]> Data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {
+                        new BigNum("2"),
+                        new List<BigFraction>() { new BigFraction(BigNum.One), new BigFraction(new BigNum("-2")), new BigFraction(BigNum.One) },
+                        new Polynomial(BigNum.One, new List<BigFraction>() { new BigFraction(BigNum.One), new BigFraction(BigNum.MinusOne) })
+                    }
+                };
+            }
+        }
+    }
+    
+
+}
