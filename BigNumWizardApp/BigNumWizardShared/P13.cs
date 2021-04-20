bf3448efac40ce789eb24d2adcaae43efa2ebf56
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-20 19:29:16 +0300

    add P13

---
+++ BigNumWizardApp/BigNumWizardShared/P13.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BigNumWizardShared
+{
+    public class P13
+    {
+        public static Polynomial NMR_P_P(BigNum m, List<BigFraction> C)
+        {
+            var C_der = P12.DER_P_P(m, C);
+            var devider = P11.GCF_PP_P(m, C, C_der.SeniorDegree, C_der.Odds);
+            var result = P9.DIV_PP_P(m, C, devider.SeniorDegree, devider.Odds);
+            return result;
+        }
+    }
+}
