60b7868f1f3cda7c29f30be82acb7271a73d05d9
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-21 18:55:16 +0300

    Fixed tests + handling exceptions

--- BigNumWizardApp/BigNumWizardShared/Z10.cs
+++ BigNumWizardApp/BigNumWizardShared/Z10.cs
@@ -9,87 +9,89 @@ namespace BigNumWizardShared
         public static BigNum MOD_ZZ_Z(BigNum fir, BigNum sec, out BigNum remainer)  //Остаток от деления целого на целое(делитель отличен от нуля) Кабанов 0305
         {
             remainer = BigNum.Zero;
-            if (z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 2)
+            if (sec != BigNum.Zero)
             {
-                remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
+                if (z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 2)
+                {
+                    remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
 
-                if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
+                    if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
+                        return BigNum.Zero;
+                    else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
+                    {
+                        return fir;
+                    }
+                    else
+                    {
+                        remainer = Z8.MUL_ZZ_Z(sec, remainer);
+                        remainer = Z7.SUB_ZZ_Z(fir, remainer);
+                        return remainer;
+                    }
+                }
+                else if (z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 2)
+                {
+                    remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
+
+                    if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
+                        return BigNum.Zero;
+                    else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
+                    {
+                        remainer = Z7.SUB_ZZ_Z(fir, fir + fir);
+                        return remainer;
+                    }
+                    else
+                    {
+
+                        remainer = Z8.MUL_ZZ_Z(sec, remainer + BigNum.MinusOne);
+                        remainer = Z7.SUB_ZZ_Z(fir, remainer);
+
+                        return remainer;
+                    }
+                }
+                else if (z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 1)
+                {
+
+                    remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
+
+                    if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
+                        return BigNum.Zero;
+                    else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
+                    {
+                        return fir;
+                    }
+                    else
+                    {
+                        remainer = Z8.MUL_ZZ_Z(sec, remainer);
+
+                        remainer = Z7.SUB_ZZ_Z(fir, remainer);
+                        return remainer;
+                    }
+
+                }
+                else if (z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 1)
+                {
+                    remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
+
+                    if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
+                        return BigNum.Zero;
+                    else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
+                    {
+                        remainer = Z7.SUB_ZZ_Z(fir, fir + fir);
+                        return remainer;
+                    }
+                    else
+                    {
+
+                        remainer = Z8.MUL_ZZ_Z(sec, remainer + BigNum.One);
+                        remainer = Z7.SUB_ZZ_Z(fir, remainer);
+
+                        return remainer;
+                    }
+                }
+                else 
                     return BigNum.Zero;
-                else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
-                {
-                    return fir;
-                }
-                else
-                {
-                    remainer = Z8.MUL_ZZ_Z(sec, remainer);
-                    remainer = Z7.SUB_ZZ_Z(fir, remainer);
-                    return remainer;
-                }
             }
-            else if (z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 2)
-            {
-                remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
-
-                if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
-                    return BigNum.Zero;
-                else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
-                {
-                    //remainer = z2_3.MUL_ZM_Z(fir);
-                    remainer = Z7.SUB_ZZ_Z(fir, fir + fir);
-                    return remainer;
-                }
-                else
-                {
-
-                    remainer = Z8.MUL_ZZ_Z(sec, remainer + BigNum.MinusOne);
-                    remainer = Z7.SUB_ZZ_Z(fir, remainer);
-
-                    return remainer;
-                }
-            }
-            else if (z2_3.POZ_Z_D(fir) == 2 && z2_3.POZ_Z_D(sec) == 1)
-            {
-
-                remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
-
-                if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
-                    return BigNum.Zero;
-                else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
-                {
-                    return fir;
-                }
-                else
-                {
-                    remainer = Z8.MUL_ZZ_Z(sec, remainer);
-
-                    remainer = Z7.SUB_ZZ_Z(fir, remainer);
-                    return remainer;
-                }
-
-            }
-            else if (z2_3.POZ_Z_D(fir) == 1 && z2_3.POZ_Z_D(sec) == 1)
-            {
-                remainer = Z9.DIV_ZZ_Z(fir, sec, out _);
-
-                if (Z8.MUL_ZZ_Z(remainer, sec) == fir)
-                    return BigNum.Zero;
-                else if (Z9.DIV_ZZ_Z(fir, sec, out _) == BigNum.Zero)
-                {
-                    //remainer = z2_3.MUL_ZM_Z(fir);
-                    remainer = Z7.SUB_ZZ_Z(fir, fir + fir);
-                    return remainer;
-                }
-                else
-                {
-
-                    remainer = Z8.MUL_ZZ_Z(sec, remainer + BigNum.One);
-                    remainer = Z7.SUB_ZZ_Z(fir, remainer);
-
-                    return remainer;
-                }
-            }
-            else if (z2_3.POZ_Z_D(fir) == 0)
-                return BigNum.Zero;
+            
             else
                 throw new Exception("На ноль делить нельзя!");
         }
