43e2437a6b19cf32ba5600c0f57bf1bee61b7015
osiptsoff <osipczovnikita@gmail.com>
2021-04-21 17:07:05 +0300

    Q5 and Q7 - zero division exception added

--- BigNumWizardApp/BigNumWizardShared/Q5_7.cs
+++ BigNumWizardApp/BigNumWizardShared/Q5_7.cs
@@ -1,15 +1,27 @@
-﻿namespace BigNumWizardShared
+﻿using System;
+
+namespace BigNumWizardShared
 {
     public class Q5_7
     {
         public static BigFraction ADD_QQ_Q(BigFraction fir, BigFraction sec)    // Q-5, сложение дробей, Осипцов Никита 0305
         {
-            var result = fir + sec; // Получить несокращённую дробь-сумму
-
-            result = Q1.RED_Q_Q(result);
-            return result;
+            if (fir.Denom != BigNum.Zero && sec.Denom != BigNum.Zero)
+            {
+                var result = fir + sec; // Получить несокращённую дробь-сумму
+                result = Q1.RED_Q_Q(result);
+                return result;
+            }
+            else
+                throw new Exception("На ноль делить нельзя!");
         }
        
-        public static BigFraction MUL_QQ_Q(BigFraction fir, BigFraction sec) { return fir * sec; } // Q-7, умножение дробей, Осипцов Никита 0305
+        public static BigFraction MUL_QQ_Q(BigFraction fir, BigFraction sec) // Q-7, умножение дробей, Осипцов Никита 0305
+        {
+            if (fir.Denom != BigNum.Zero && sec.Denom != BigNum.Zero)
+                return fir * sec;
+            else
+                throw new Exception("На ноль делить нельзя!");
+        }
     }
 }
