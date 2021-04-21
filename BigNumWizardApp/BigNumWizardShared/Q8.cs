d265c4353a0afd20609202b4f7e5c5d08f62dd20
Марина Петракова <mrpetrakova2000@gmail.com>
2021-04-21 19:03:31 +0300

    Fixed handling exceptions

--- BigNumWizardApp/BigNumWizardShared/Q8.cs
+++ BigNumWizardApp/BigNumWizardShared/Q8.cs
@@ -7,7 +7,7 @@ namespace BigNumWizardShared
         public static BigFraction DIV_QQ_Q(BigFraction first, BigFraction second) //Q-8 Деление дробей Петракова Марина 0305 
         {
             BigFraction fraction;
-            if (first.Denom != BigNum.Zero && second.Nom != BigNum.Zero)
+            if (first.Denom != BigNum.Zero && second.Nom != BigNum.Zero && second.Denom != BigNum.Zero)
             {
                 fraction = first / second;
             }
